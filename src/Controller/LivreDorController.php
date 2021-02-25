2aa73205e48bed2aa1f22f2b0f8d6c1bb2d4fe04
Thaishery <gdeb@gdeb.fr>
2021-02-25 16:24:52 +0100

    carousel

---
+++ src/Controller/LivreDorController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LivreDorController extends AbstractController
+{
+    /**
+     * @Route("/livre/dor", name="livre_dor")
+     */
+    public function index(): Response
+    {
+        return $this->render('livre_dor/index.html.twig', [
+            'controller_name' => 'LivreDorController',
+        ]);
+    }
+}
