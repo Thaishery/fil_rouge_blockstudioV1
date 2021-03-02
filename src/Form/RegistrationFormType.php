d535bf0d48d477e3d54ca16ca250483a40fa8bb0
Thaishery <gdeb@gdeb.fr>
2021-03-02 16:28:51 +0100

    regex: WIP // réagencement des formulaires : WIP

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,25 +5,53 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('login')
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+             ->add('lastname', TextType::class, [
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^[A-Z][\p{L}-]*$/',
+                        'message' => 'Votre nom est invalide, il ne dois pas comporter de nombres ou d\'espace et commencer par une majuscule.',
+                    ]),
+                ],
+            ])
+             ->add('firstname', TextType::class, [
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^[A-Z][\p{L}-]*$/',
+                        'message' => 'Votre prénom est invalide, il ne dois pas comporter de nombres ou d\'espace et commencer par une majuscule.',
+                    ]),
+                ],
+            ])
+             ->add('login')
+             ->add('email', EmailType::class,[
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^\w*[a-zA-Z0-9-_âêîôûäëïöüéèàçÂÊÎÔÛÄËÏÖÜÀÆæÇÉÈŒœÙ]*+@\w*[a-zA-Z0-9-_âêîôûäëïöüéèàçÂÊÎÔÛÄËÏÖÜÀÆæÇÉÈŒœÙ]*+\.\w+$/',
+                        'message' => 'Email invalide'
+                    ])
+                ]
+            ])
+             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Veuillez accepter les conditions',
                     ]),
                 ],
             ])
@@ -33,18 +61,39 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe.',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit au minimum contenir {{ limit }} caractères.',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+                    // regex caractére spécial : 
+                    new Assert\Regex([
+                        'pattern' => '/[^A-Za-z0-9]+/',
+                        'message' => 'Vous devez saisir au moins 1 caractére spécial.'
+                    ]),
+                    //regex Majuscule : 
+                    new Assert\Regex([
+                        'pattern' => '/[A-Z]+/',
+                        'message' => 'Vous devez saisir au moins 1 Majuscule.'
                     ]),
                 ],
             ])
         ;
     }
+
+
+
+
+
+
+
+
+
+
+
 
     public function configureOptions(OptionsResolver $resolver)
     {
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,12 +6,16 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 // use Symfony\Component\Validator\Constraints\Choice;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints as Assert;
+use Webmozart\Assert\Assert as AssertAssert;
 
 class UserType extends AbstractType
 {
@@ -42,11 +46,42 @@ class UserType extends AbstractType
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+                    // regex caractére spécial : 
+                    new Assert\Regex([
+                        'pattern' => '/[^A-Za-z0-9]+/',
+                        'message' => 'Vous devez saisir au moins 1 caractére spécial.'
+                    ]),
+                    //regex Majuscule : 
+                    new Assert\Regex([
+                        'pattern' => '/[A-Z]+/',
+                        'message' => 'Vous devez saisir au moins 1 Majuscule.'
+                    ])
                 ],
             ])
-            ->add('email', EmailType::class)
-            ->add('firstname')
-            ->add('lastname')
+            ->add('email', EmailType::class,[
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^\S+@\S+\.\S+$/',
+                        'message' => 'Email invalide'
+                    ])
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^[A-Z][\p{L}-]*$/',
+                        'message' => 'Votre prénom est invalide, il ne dois pas comporter de nombres ou d\'espace et commencer par une majuscule.',
+                    ]),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^[A-Z][\p{L}-]*$/',
+                        'message' => 'Votre nom est invalide, il ne dois pas comporter de nombres ou d\'espace et commencer par une majuscule.',
+                    ]),
+                ],
+            ])
             ->add('address')
             ->add('phone')
             //->add('avatar')
