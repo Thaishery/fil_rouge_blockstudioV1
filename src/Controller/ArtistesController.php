86b4a18d1acccd49196d6f69bcc45631a3f283da
Thaishery <gdeb@gdeb.fr>
2021-03-24 16:33:43 +0100

    wip on cardFlip on Les Artistes

--- src/Controller/ArtistesController.php
+++ src/Controller/ArtistesController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Projet;
+use Knp\Component\Pager\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +15,15 @@ class ArtistesController extends AbstractController
     /**
      * @Route("/artistes", name="artistes")
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
-        $projetList = $this->getDoctrine()->getRepository(Projet::class)->findAll();
+        // $projetList = $this->getDoctrine()->getRepository(Projet::class)->findAll();
+        $data = $this->getDoctrine()->getRepository(Projet::class)->findAll();
+        $projetList = $paginator->paginate(
+            $data,
+            $request->query->getInt('page',1),
+            12
+        );
         return $this->render('artistes/index.html.twig', [
             'controller_name' => 'ArtistesController',
             'projet' => $projetList,
