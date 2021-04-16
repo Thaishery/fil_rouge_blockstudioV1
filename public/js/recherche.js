fa742d8c7071aa3627558935fa6287bbdc8b7288
Thaishery <gdeb@gdeb.fr>
2021-04-16 12:08:01 +0200

    modif sur la barre de recherche

---
+++ public/js/recherche.js
@@ -0,0 +1,10 @@
+var Label = document.getElementById("search_projet_Rechercher");
+var Title = document.getElementById("h1_projets");
+var Recherche = document.getElementById("searchbar");
+Label.innerHTML = '<i class="fas fa-search"></i>';
+//_gd fonctionne mais pas tout a fait (a modifier!)
+// document.getElementById("search_projet_Rechercher").addEventListener("mouseover",resize)
+// function resize(){
+//     Recherche.className += " col-6";
+//     Title.className += " col-6";
+// }
