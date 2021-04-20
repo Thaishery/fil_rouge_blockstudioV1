323e29a67af85e3abc22b38ca97ed66bb7c3caba
jordancarmont <biziboy91@gmail.com>
2021-04-21 00:20:50 +0200

    Modif sur H1 - Modif map contact -Ajout + Modif photo carousel

--- public/js/recherche.js
+++ public/js/recherche.js
@@ -2,6 +2,8 @@ var Label = document.getElementById("search_projet_Rechercher");
 var Title = document.getElementById("h1_projets");
 var Recherche = document.getElementById("searchbar");
 Label.innerHTML = '<i class="fas fa-search"></i>';
+
+
 //_gd fonctionne mais pas tout a fait (a modifier!)
 // document.getElementById("search_projet_Rechercher").addEventListener("mouseover",resize)
 // function resize(){
